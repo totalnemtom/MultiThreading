0986062b6deabcd695bbeee7226301c94575976b
totalnemtom <matybal.m@gmail.com>
2021-05-28 00:18:23 +0200

    2 thread

--- src/multithreading/MultiThreading.java
+++ src/multithreading/MultiThreading.java
@@ -1,7 +1,24 @@
 package multithreading;
 
+class Tasker extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Other thread: " + i);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+}
+
 public class MultiThreading {
     public static void main(String[] args) {
-
+        Tasker run1 = new Tasker();
+        run1.start();
+        Tasker run2 = new Tasker();
+        run2.start();
     }
 }
